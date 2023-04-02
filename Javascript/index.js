a8e4b66150707ebef17940ffb73adc356973ce24
kevindiaz15 <polsion155@gmail.com>
2023-04-01 22:15:30 -0500

    funciond de usuario de prueba y comentarios

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,14 +1,28 @@
- var nombreUsuario = "usuarioprueba123";
- var contrasena = "12345";
+var nombreUsuario = "usuarioprueba123";
+var clave = "12345";
 
- function autenticacion(usuario, ingreseContrasena) {
-     usuario; 
-    ingreseContrasena;
+// function autenticacion(usuario, ingreseContrasena) {
+//     usuario; 
+//     ingreseContrasena;
 
-     if (nombreUsuario===usuario) {
-        alert ("autenticacion correcta");
-        }else if (nombreUsuario != usuario || contrasena != ingreseContrasena) {
+//     if (nombreUsuario===usuario) {
+//         alert ("autenticacion correcta");
+//         }else if (nombreUsuario != usuario || contrasena != ingreseContrasena) {
 
-         }
- }
- autenticacion(prompt("ingrese usuario"),prompt("ingreseContrasena"));
+//         }
+// }
+// autenticacion(prompt("ingrese usuario"),prompt("ingreseContrasena"));
+
+
+function ingreso() {
+    
+    let usuario = document.getElementById("usuarioinput").value;
+    let password = document.getElementById("contrainput").value;
+    
+    if (nombreUsuario===usuario && clave === password) {
+        alert ("ingreso exitoso");
+        location.href = "../pages/home.html"
+    }else if (nombreUsuario != usuario || contrasena != ingreseContrasena) {
+        alert ("error en el ingreso")
+        }
+}
