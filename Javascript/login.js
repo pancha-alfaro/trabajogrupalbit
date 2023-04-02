4406b9cf31a0b82848a5f0c170a8dff36f48ac7f
kevindiaz15 <polsion155@gmail.com>
2023-04-01 23:17:23 -0500

    correcciones

--- Javascript/login.js
+++ Javascript/login.js
@@ -1,17 +1,6 @@
 var nombreUsuario = "usuarioprueba123";
 var clave = "12345";
 
-// function autenticacion(usuario, ingreseContrasena) {
-//     usuario; 
-//     ingreseContrasena;
-
-//     if (nombreUsuario===usuario) {
-//         alert ("autenticacion correcta");
-//         }else if (nombreUsuario != usuario || contrasena != ingreseContrasena) {
-
-//         }
-// }
-// autenticacion(prompt("ingrese usuario"),prompt("ingreseContrasena"));
 
 
 function ingreso() {
